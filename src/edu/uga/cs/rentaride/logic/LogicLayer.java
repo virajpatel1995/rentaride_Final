38b273b8ca13429daa86aba393f22acc8cdec021
Viraj Patel <patelviraj2@yahoo.com>
2017-11-02 21:32:13 -0400

    Eaten

--- src/edu/uga/cs/rentaride/logic/LogicLayer.java
+++ src/edu/uga/cs/rentaride/logic/LogicLayer.java
@@ -13,17 +13,7 @@ import edu.uga.cs.rentaride.RARException;
 public interface LogicLayer
 {
 	
-	/*
-    public List<Rental>         findAllRentals() throws RARException;
-    public List<Customer>       findAllCustomers() throws RARException;
-    public long               joinClub( Customer customer, Club club ) throws RARException;
-    public long               joinClub( long customerId, String clubName ) throws RARException;
-    public long               createClub( String clubName, String address, long founderId ) throws RARException;
-    public long               createCustomer( String firstName, String lastName, String userName, String password, String email,  
-                                            String address, String licState, String licNumber, String ccNumber, String ccExpiration ) throws RARException;
-    public List<Customer>       findClubMembers( String clubName ) throws RARException;
-   */
-	//public List<Administrator> findAllAdministrators() throws RARException;
+
 	public void               logout( String ssid ) throws RARException;
     public String             login( Session session, String userName, String password ) throws RARException;
 	
