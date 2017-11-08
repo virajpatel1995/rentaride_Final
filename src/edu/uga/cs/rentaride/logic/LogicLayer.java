c1b3c5a8daf6b55e34776862f9c880a86066f343
Junweiren <junweir@gmail.com>
2017-11-07 23:31:40 -0500

    made changes to registerAccount and other places. Got register customer working

--- src/edu/uga/cs/rentaride/logic/LogicLayer.java
+++ src/edu/uga/cs/rentaride/logic/LogicLayer.java
@@ -1,6 +1,7 @@
 package edu.uga.cs.rentaride.logic;
 
 
+import java.text.ParseException;
 import java.util.List;
 
 import edu.uga.cs.rentaride.RARException;
@@ -14,7 +15,7 @@ public interface LogicLayer
 {
 	
 
-	public long registerAccount ( String fName, String lName, String email, String password, String driverNo, String cardNo, String expDate, String address, String state, String zip ) throws RARException;
+	public long registerAccount ( String fName, String lName, String email, String password, String username, String driverNo, String cardNo, String expDate, String address, String state, String zip ) throws RARException, ParseException;
 
 	public void               logout( String ssid ) throws RARException;
     public String             login( Session session, String userName, String password ) throws RARException;
