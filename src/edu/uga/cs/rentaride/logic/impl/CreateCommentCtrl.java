f43647aaa93e747c05ccc7f8c3d139501256290d
Jambrose777 <Jaa74458@uga.edu>
2017-11-16 18:37:36 -0500

    Create Comment Ctrl

---
+++ src/edu/uga/cs/rentaride/logic/impl/CreateCommentCtrl.java
@@ -0,0 +1,54 @@
+package edu.uga.cs.rentaride.logic.impl;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.Date;
+import java.util.List;
+
+import edu.uga.cs.rentaride.RARException;
+import edu.uga.cs.rentaride.entity.Administrator;
+import edu.uga.cs.rentaride.entity.Comment;
+import edu.uga.cs.rentaride.entity.Rental;
+import edu.uga.cs.rentaride.entity.User;
+import edu.uga.cs.rentaride.entity.UserStatus;
+import edu.uga.cs.rentaride.object.ObjectLayer;
+
+public class CreateCommentCtrl {
+
+private ObjectLayer objectLayer = null;
+	
+    public CreateCommentCtrl( ObjectLayer objectModel ){
+        this.objectLayer = objectModel;
+    }
+    
+	public long createComment( String comm, String ren) throws RARException{
+
+		//cheack if customer exists
+		Rental modelRental = objectLayer.createRental();
+        modelRental.setId(Long.parseLong(ren));
+        List<Rental> rentals = objectLayer.findRental( modelRental );
+
+
+        if( rentals.size() > 0 ) {
+        
+        	//Creating the comment
+	   Rental rental = rentals.get(0);
+			
+	   Comment comment = null;
+	   comment  = objectLayer.createComment(comm, new Date(), rental, rental.getCustomer());
+	    
+	    	objectLayer.storeComment(comment);
+	    
+
+	    	return comment.getId();
+	    	
+        }else{
+        		throw new RARException("SessionManager.CreateComment: Invalid Rental Location ");
+        }
+
+		
+	    	
+    }
+}
+
