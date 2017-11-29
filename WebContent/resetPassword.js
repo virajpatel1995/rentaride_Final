692da2377536f506a0ea5648e2058c583a9a75cc
Viraj Patel <patelviraj2@yahoo.com>
2017-11-28 22:00:45 -0500

    Fixed reset Password

--- WebContent/resetPassword.js
+++ WebContent/resetPassword.js
@@ -6,11 +6,15 @@
 $(document).ready(function() {
         $('#submitP').click(function(event) {
                 var username = $('#username').val();
-                var email = $('#email').val();
+               // var email = $('#email').val();
+                var firstName = $("#firstName").val();
+                var lastName = $("#lastName").val();
                 var pass = $('#pass').val();
                 $.get('ResetPassword', {
                         username : username,
-                        email : email,
+                        //email : email,
+                        firstName : firstName,
+                        lastName : lastName,
                         pass : pass
                         
                 }, function(responseText) {
