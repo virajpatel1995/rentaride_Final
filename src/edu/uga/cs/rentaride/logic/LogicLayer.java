ed6836714c725f63d3c917452afad35097ab89c8
Jambrose777 <Jaa74458@uga.edu>
2017-11-29 21:47:59 -0500

    place Reservation

--- src/edu/uga/cs/rentaride/logic/LogicLayer.java
+++ src/edu/uga/cs/rentaride/logic/LogicLayer.java
@@ -48,4 +48,5 @@ public interface LogicLayer
     void updateRentalLocation(RentalLocation rl) throws RARException;
 	public long CancelReservation(String res) throws RARException;
 	public long placeRental(String reservationIdS, String vehicleTag) throws RARException;
+	public long placeReservation(String time, String dur, String loc, String type, String uName) throws RARException ;
 }
